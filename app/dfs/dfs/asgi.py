f311336f9abbfb8c88a01eeada7b07d6e4be9a73
Oleksii Kutsenko <kutsenko.new@gmail.com>
2023-09-24 21:42:08 +0200

    WIP

--- app/dfs/dfs/asgi.py
+++ app/dfs/dfs/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dfs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfs.settings")
 
 application = get_asgi_application()
--- app/dfs/dfs/wsgi.py
+++ app/dfs/dfs/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dfs.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfs.settings")
 
 application = get_wsgi_application()
